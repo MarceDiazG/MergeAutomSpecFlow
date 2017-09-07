2ade3bb5d0f8f8ce6d3f92696d2cb72a691cd8aa
MarceDiazG <marcelo.diaz@globant.com>
2017-09-06 21:56:25 -0300

    Commit including: Added SpecFlow (Nuget reference, Story LoginEducator, LoginEducatorSteps)

---
+++ ClassLibrary2/Steps/LoginEducatorSteps.cs
@@ -0,0 +1,35 @@
+﻿using Edgenuity.Educator.PageObject;
+using GuiAutomationFramework.Framework.PageObject;
+using System;
+using TechTalk.SpecFlow;
+
+namespace ClassLibrary2.Steps
+{
+    [Binding]
+    public class LoginEducatorSteps{
+
+        public LoginEducatorSteps(){
+            LoginPage loginPage = new LoginPage(); // PageFactoryHelper.GetPage(LoginPage);
+        }
+
+        [Given(@"Going to Educator Portal")]
+        public void GivenGoingToEducatorPortal()
+        {
+
+            
+            //ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I complete credentials Marceloadmin and marcelo")]
+        public void WhenICompleteCredentialsMarceloadminAndMarcelo()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The educator is succesfully logged")]
+        public void ThenTheEducatorIsSuccesfullyLogged()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ GuiAutomationFramework/EdgeTestClass.cs
@@ -0,0 +1,26 @@
+﻿using GuiAutomationFramework.Framework.Driver.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace GuiAutomationFramework
+{
+    public class EdgeTestClass
+    {
+        [BeforeScenario]
+        public void SetUp()
+        {
+            DriverFactory.NewInstance(Framework.Enums.Browsers.Firefox, false);
+        }
+
+        [AfterScenario]
+        public void TearDown()
+        {
+            CloseDriver.Close();
+            //Driver.Close();
+        }
+    }
+}
